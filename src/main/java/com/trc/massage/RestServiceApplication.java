d7cfc67b4de6b94df2d7c34fe3fb0e8fdf55d396
spicon <spicon@travelcompositor.com>
2024-01-09 19:50:57 +0100

    Algunos masajes mas

--- src/main/java/com/trc/massage/RestServiceApplication.java
+++ src/main/java/com/trc/massage/RestServiceApplication.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
@@ -27,7 +28,7 @@ class MassageController {
 
     @GetMapping("/massages/quote/{date}")
     public MassageResponse quoteMassage(@PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate date) {
-        if (date.isBefore(LocalDate.now().minusDays(15))) {
+        if (date.isBefore(LocalDate.now().plusDays(15))) {
             return new MassageResponse("La antelación mínima para reservar es de 15 días");
         }
         var massages = buildMassages(date);
@@ -37,50 +38,58 @@ class MassageController {
     private static List<Massage> buildMassages(LocalDate date) {
         var massage1 = new Massage("1"
                 , "Masaje sueco"
-                , buildStatus()
+                , "AVAILABLE"
                 , new Price(25.0, "EUR")
                 , ofMinutes(30)
                 , List.of(new CancellationPolicy(date.minusDays(5), new Price(15.0, "EUR"))
                 , new CancellationPolicy(date.minusDays(6), new Price(30.0, "EUR"))));
         var massage2 = new Massage("2"
-                , "Masaje de Aromaterapia"
+                , "Masaje de Aromaterapia con luces de colores"
                 , "ON_REQUEST"
-                , new Price(50.0, "EUR")
+                , new Price(300.0, "EUR")
                 , ofMinutes(85)
-                , List.of(new CancellationPolicy(date.minusDays(15), "25")
-                , new CancellationPolicy(date.minusDays(5), "50")
-                , new CancellationPolicy(date.minusDays(2), "100")));
+                , Collections.emptyList());
         var massage3 = new Massage("3"
                 , "Masaje Deportivo 60 minutos"
-                , buildStatus()
+                , "AVAILABLE"
                 , new Price(60.0, "EUR")
                 , ofMinutes(60)
                 , List.of(new CancellationPolicy(date.minusDays(3), "50")
                 , new CancellationPolicy(date.plusDays(1), "100")));
         var massage4 = new Massage("4"
                 , "Masaje Deportivo 45 minutos"
-                , buildStatus()
+                , "AVAILABLE"
                 , new Price(45.0, "EUR")
                 , ofMinutes(45)
                 , List.of(new CancellationPolicy(date.minusDays(10), "0")
                 , new CancellationPolicy(date, "100")));
         var massage5 = new Massage("5"
+                , "Masaje de piedras calientes0"
+                ,  "AVAILABLE"
+                , new Price(150.0, "EUR")
+                , ofMinutes(70)
+                , List.of(new CancellationPolicy(date.minusDays(365), new Price(150.0, "EUR"))));
+        var massage6 = new Massage("5"
                 , "Masaje Tailandés"
-                , buildStatus()
+                ,  "UNAVAILABLE"
                 , new Price(100.0, "EUR")
                 , ofMinutes(70)
                 , List.of(new CancellationPolicy(date.minusDays(365), new Price(100.0, "EUR"))));
-        var massage6 = new Massage("6"
+        var massage7 = new Massage("6"
                 , "Masaje Shiatsu"
                 , "DISABLED"
                 , new Price(150.0, "EUR")
                 , ofMinutes(90)
                 , List.of(new CancellationPolicy(date.minusDays(365), new Price(0.0, "EUR"))));
-        return List.of(massage1, massage2, massage3, massage4, massage5, massage6);
-    }
-
-    private static String buildStatus() {
-        return new Random().nextInt(10) == 5 ? "UNAVAILABLE" : "AVAILABLE";
+        var massage8 = new Massage("2"
+                , "Masaje de Aromaterapia"
+                , "AVAILABLE"
+                , new Price(50.0, "EUR")
+                , ofMinutes(85)
+                , List.of(new CancellationPolicy(date.minusDays(15), "25")
+                , new CancellationPolicy(date.minusDays(5), "50")
+                , new CancellationPolicy(date.minusDays(2), "100")));
+        return List.of(massage1, massage2, massage3, massage4, massage5, massage6, massage7, massage8);
     }
 }
 
